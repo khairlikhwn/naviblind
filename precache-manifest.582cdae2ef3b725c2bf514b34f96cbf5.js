b24965a24ad7e853153f0170b1cd088c5b3bd32f
khairlikhwn <khairulmn83@gmail.com>
2023-10-22 16:11:13 +0800

    Updates

---
+++ precache-manifest.582cdae2ef3b725c2bf514b34f96cbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5e69db13d99fe21adef410b32afb91",
+    "url": "/naviblind/index.html"
+  },
+  {
+    "revision": "8ea53e18e9e797398bff",
+    "url": "/naviblind/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0986ed681495842c362c",
+    "url": "/naviblind/static/js/2.2c458268.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/naviblind/static/js/2.2c458268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea53e18e9e797398bff",
+    "url": "/naviblind/static/js/main.ba813dbf.chunk.js"
+  },
+  {
+    "revision": "9000a3e5cd8e798ce9dd",
+    "url": "/naviblind/static/js/runtime-main.add96069.js"
+  }
+]);
